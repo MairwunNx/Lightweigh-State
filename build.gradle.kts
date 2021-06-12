3da921c2ff1026a7f13ddf46370b0c91e5b0670a
Pavel Erokhin <MairwunNx@gmail.com>
2021-06-12 16:05:56 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "com.mairwunnx"
+version = "1.0.0-SNAPSHOT.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
